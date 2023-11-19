74b5875345b65d0ae3c67f2818599582cc8cea4f
sabinelohnerv <sabinelohnerv@gmail.com>
2023-11-19 10:40:43 -0400

    items interactor update

--- lib/modules/items/interactor/items_interactor.dart
+++ lib/modules/items/interactor/items_interactor.dart
@@ -5,7 +5,7 @@ class ItemsInteractor {
     return [
       Item(id: 1, name: "Formal Shirt", image: 'shirt', price: '35.99'),
       Item(id: 2, name: "Knitted Sweater", image: 'sweater', price: '59.89'),
-      Item(id: 3, name: "Curdoroy Pants", image: 'pants', price: '75.99'),
+      Item(id: 3, name: "Corduroy Pants", image: 'pants', price: '75.99'),
       Item(id: 4, name: "Bodycon Dress", image: 'dress', price: '32.89'),
       Item(id: 5, name: "Teddy Pullover", image: 'pullover', price: '46.99'),
     ];
